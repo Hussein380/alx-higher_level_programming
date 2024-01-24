cea66b629486bd28782330ed7b5df25cc3f676dd
Hussein380 <huznigarane@gmail.com>
2024-01-23 18:06:58 -0800

    changed output to be exact

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,7 @@ class Square:
         """
 
         if not isinstance(value, int):
-            raise TypeError("size must be an intager")
+            raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
