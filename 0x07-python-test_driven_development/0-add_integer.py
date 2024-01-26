2185a13a185765aff875b1edc9405f32b5738783
Hussein380 <huznigarane@gmail.com>
2024-01-26 03:41:46 -0800

    Created function def add_integer(a, b=98): that adds 2 intagers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Adds two integers : a and b
+
+    Args:
+        a(int or float): The first operand
+        b(int or float): The seconfd operand
+
+    Returns:
+         int, the sum of a and b
+
+    Raises:
+        TypeError: if a or b is not an integer or float
+
+    """
+
+    #check if a is not an int or float
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+
+    #check if b is not  an int or float
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+
+    #cast a and b intagers if they are float
+
+    a  = int(a)
+    b = int(b)
+
+    #return the sum
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+This  directory is for tasks that covers Python - Test-driven development
