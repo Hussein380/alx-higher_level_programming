41f34bb17d8c764b663ad9fc8207a181db71d17d
Hussein380 <huznigarane@gmail.com>
2024-01-28 23:41:40 -0800

    Created an empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This modules introduces class rectangle"""
+
+
+class Rectangle:
+    """
+    The class Rectangle is empty
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This directory covers ALX's Python - More Classes and Objects. The directory covers tasks for Python - More Classes and Objects
