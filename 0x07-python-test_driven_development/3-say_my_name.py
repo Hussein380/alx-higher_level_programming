edd7db5db1ced03aed8a09cdbeaf06277141d2d6
Hussein380 <huznigarane@gmail.com>
2024-01-31 02:56:42 -0800

    corrected pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a name-printing fucntion"""
 
+
 def say_my_name(first_name, last_name=""):
     """
     print a  name
@@ -11,11 +12,21 @@ def say_my_name(first_name, last_name=""):
 
     Raises:
         TypeError: If either of first_name or last_name are not strings
+
+    Examples:
+        >>> say_my_name("John", "Doe")
+        My name is John Doe
+
+        >>> say_my_name("Alice")
+        My name is Alice
+
+        >>> say_my_name("42", "Smith")
+        Error: first_name must be a string
     """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
-
