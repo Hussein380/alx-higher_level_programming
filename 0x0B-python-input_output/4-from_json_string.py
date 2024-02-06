feee40c55b4501b5603c202c098b1b1a9acad966
Hussein380 <huznigarane@gmail.com>
2024-02-06 03:26:27 -0800

    function that returns an object (Python data structure) represented by a JSON string:

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""defines object represented by a JSON dtring"""
+
+
+import  json
+def from_json_string(my_str):
+    """
+    returns an object (Python data structure) represented by a JSON string:
+
+    Args:
+        my_str: The json string to decerialize
+
+    Returns: object represented by JSON string
+    """
+    return json.loads(my_str) 
