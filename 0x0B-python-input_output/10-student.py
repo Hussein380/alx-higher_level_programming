b4cefc08b29038292bd57d056c23684270b24c49
Hussein380 <huznigarane@gmail.com>
2024-02-06 06:29:49 -0800

    mplemented to_json method in Student class for generating dictionary representation with optional attribute filtering

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Defines a class studemt"""
+
+
+class Student:
+    """epresent a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new student
+
+        Args:
+            first_name (str): The name of te student
+            last_name (str): The last name of the student
+            age (int): The age of te student
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the student
+        If attrs is a list of strings, represents only those attributes
+        inlude in the list
+
+        Args:
+            attr (list): (optional) The attributes to represent
+
+        """
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr
+                                           in attrs):
+
+            return {key: value for key, value in self.__dict__.items() if key
+                    in attrs}
+        return self.__dict__
