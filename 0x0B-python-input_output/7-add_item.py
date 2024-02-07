1de716e0435bcb88050d417ce68a0df2095390e8
Hussein380 <huznigarane@gmail.com>
2024-02-07 00:13:08 -0800

    corrected the logic of the code to match the desired output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Add all arguments to a python list and save them to a file"""
-
-
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+
 
 if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
     try:
         items = load_from_json_file("add_item.json")
 
@@ -17,4 +18,4 @@ if __name__ == "__main__":
     items.extend(sys.argv[1:])
 
     """save the update list to the file """
-    save_to_json_file(items "add_item.json")
+    save_to_json_file(items, "add_item.json")
