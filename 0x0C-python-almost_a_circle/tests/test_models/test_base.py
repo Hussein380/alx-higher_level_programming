83d97324822ec4a095fa01a61164bb77011fb21d
Hussein380 <huznigarane@gmail.com>
2024-02-09 11:33:00 -0800

    added more edge cases for the base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,18 +1,20 @@
+#!/usr/bin/python3
+""" Defines unittests foe base.py
+unittest classes:
+     TestBase_instantiation
+
+"""
 import unittest
 from models.base import Base
 
 
-class TestBase(unittest.TestCase):
+class TestBase_instantiation(unittest.TestCase):
     """unit test for the Base class"""
     def test_id_generation(self):
         """Test id generation when id is not provided """
         base1 = Base()
         base2 = Base()
-        base3 = Base()
-
-        self.assertEqual(base1.id, 1)
-        self.assertEqual(base2.id, 2)
-        self.assertEqual(base3.id, 3)
+        self.assertEqual(base1.id, base2.id - 1)
 
     def test_id_provided(self):
         """Test id assignement when id is provided"""
@@ -51,6 +53,36 @@ class TestBase(unittest.TestCase):
         base1 = Base(10)
         base2 = Base(10)
         self.assertEqual(base1.id, base2.id)
+
+    def test_no_arg(self):
+        """Test when no argument is provided"""
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base1.id, base2.id - 1)
+
+    def test_three_bases(self):
+        """Test instantation of three objects and check if their IDs are consecutive """
+        base1 = Base()
+        base2 = Base()
+        base3 = Base()
+        self.assertEqual(base1.id, base3.id -2)
+
+    def test_None_id(self):
+        """Test instantation with None as the ID"""
+        base1 = Base(None)
+        base2 = Base(None)
+        self.assertEqual(base1.id, base2.id - 1)
+
+    def unique_id(self):
+        """Test instantiation with uniques ID"""
+        assertEqual(12, Base(12).id)
+
+    def test_nb_instances_after_unique_id(self):
+        """Test if the number of instances increments correctly """
+        base1 = Base()
+        base2 = Base(12)
+        base3 = Base()
+        self.assertEqual(base1.id, base3.id - 1)
 
 if __name__ == "__main__":
     unittest.main()
