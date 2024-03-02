bb8418416fc22c0bffd31b97ef2140c55cc9df24
Hussein380 <huznigarane@gmail.com>
2024-03-02 11:49:42 +0300

    corrected pycodestyle and typos in the code

--- 0x09-python-everything_is_object/100-main.py
+++ 0x09-python-everything_is_object/100-main.py
@@ -2,7 +2,7 @@
 magic_string = __import__('100-magic_string').magic_string
 
 for i in range(10):
-        print(magic_string())#!/usr/bin/python3
+        print(magic_string())
         magic_string = __import__('100-magic_string').magic_string
 
         for i in range(10):
