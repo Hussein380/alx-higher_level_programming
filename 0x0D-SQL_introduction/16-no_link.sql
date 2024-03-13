85319bf11a08014e06a3b36586ef14a048dabba8
Hussein380 <huznigarane@gmail.com>
2024-03-13 12:19:54 +0300

    removed qquotes

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- lists all records of the table second_table having a name value in MYSQL server
 -- Record are ordered by descending score
-SELECT 'score', 'name'
-FROM 'second_table'
-WHERE 'name' != ""
-ORDER BY 'score' DESC
+SELECT score, name
+FROM second_table
+WHERE name != ""
+ORDER BY score DESC
