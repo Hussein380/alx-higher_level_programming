8e238f66c7805c0fea3b56178f9626dada7bbba9
Hussein380 <huznigarane@gmail.com>
2024-03-13 20:00:13 +0300

    added some quotes

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Creates the database hbtn_Od_2  and the user user_0d_2.
 -- creates a database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 -- Creates a user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- grants SELECT privileges to a use
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
