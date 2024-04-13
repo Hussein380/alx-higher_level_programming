2c093d15949d5095d9d93f49cec15023bcca7792
Hussein380 <huznigarane@gmail.com>
2024-04-13 12:36:01 +0300

    mandatory

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
+
 """
-Start link class to table in database
+This script lists all State objects
+from the database hbtn_0e_6_usa
 """
 
-import sys
-from model_state import Base, State
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
+    """
+    Access to the database and get states
+    from the database
+    """
 
-    """Create an SQLALchemy engine to connect to the database """
-    engine = create_engine('mysql+mysqldb://{}: \
-                           {}@localhost/{}'.format(sys.argv[1], sys.argv[2],
-                                                   sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    """Construct the database URL using command line arguments"""
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    .format(argv[1],   argv[2], argv[3])
+
+    """Create an SQLAchemy engine"""
+    engine = create_engine(db_url)
+
+    """create a configured "Session " class"""
+    Session = sessionmaker(bind=engine)
+
+    """Create a session"""
+    session = Session()
+
+    """Querry database for all the states objects, ordered by id"""
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
