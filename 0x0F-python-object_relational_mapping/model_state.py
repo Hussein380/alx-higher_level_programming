6aa37ea34523b610fc116203baa7a0c18595f6de
Hussein380 <huznigarane@gmail.com>
2024-04-18 14:47:50 +0300

    python file that contains the class definition of a State and an instance Base = declarative_base():

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
-from model_state import Base, State
 
-from sqlalchemy import create_engine
+# This script defines a SQLALchemy Base class and a State Class
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+# importing necessary modules fromSQLAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Creating a declarative base instance with the custom metadata
+Base = declarative_base()
+
+
+# Defining a class called State, which inherits from the Base class
+class State(Base):
+    """
+    Class with id and name attribute of each state
+    """
+
+    # Defining the table name in the database
+    __tablename__ = 'states'
+
+    # Defining columns for the 'states'  table
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
