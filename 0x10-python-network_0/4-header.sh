3b53ae796bd19f9220bfc2a931b34f9791e3f8b7
Hussein380 <huznigarane@gmail.com>
2024-04-25 20:14:25 +0300

    corrected typo in the code

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a GET request to the URL, displays the body of the response
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
-
+curl -sX GET $1 -H "X-School-USER-Id: 98" -L
